61f4bf84a0dbb1cae676761fc4970f85a53a9ccc
Krugw <Krugw@mail.gvsu.edu>
2017-09-27 23:23:38 -0400

    Initial commit for RnDForDummies Tool

---
+++ tools/RnDForDummies/RnDForDummies/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Xml;
+using System.IO;
+
+namespace RnDForDummies
+{
+    static class Program
+    {
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Dictionary<string, ResearchLevel> items = new Dictionary<string, ResearchLevel>();
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            string line;
+			// Read the file and display it line by line.
+			//File.SetAttributes(Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Application.ExecutablePath + "\\ResearchLevels.txt")))), FileAttributes.Normal);
+			System.IO.StreamReader file = new System.IO.StreamReader(Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Application.ExecutablePath + "\\ResearchLevels.txt")))));
+            while ((line = file.ReadLine()) != null)
+            {
+                items.Add(line.Trim(), new ResearchLevel(file.ReadLine()));
+            }
+            file.Close();
+            string JsonString = JsonConvert.SerializeObject(items, Newtonsoft.Json.Formatting.Indented);
+            System.IO.File.WriteAllText(Application.ExecutablePath, JsonString);
+
+            var a = JsonConvert.SerializeObject(items.ToArray());
+            XmlDocument xmlDocument = new XmlDocument();
+            
+            file.Close();
+        }
+    }
+}
---
+++ tools/RnDForDummies/RnDForDummies/ResearchLevel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RnDForDummies
+{
+    class ResearchLevel
+    {
+        public int Materials;
+        public int Engineering;
+        public int Plasma;
+        public int Power;
+        public int Bluespace;
+        public int Biological;
+        public int Combat;
+        public int EMP;
+        public int Data;
+        public int Illegal;
+        public int Abductor;
+
+        public ResearchLevel(string line)
+        {
+            string[] researchLevels = line.Split(';');
+            foreach(string researchLevel in researchLevels)
+            {
+                string[] level = researchLevel.Split('=');
+                ResearchType type = (ResearchType)Convert.ToInt32(level[0]);
+                if (type == ResearchType.Materials)
+                    Materials = Convert.ToInt32(level[1]);
+                else if (type == ResearchType.Engineering)
+                    Engineering = Convert.ToInt32(level[1]);
+                else if (type == ResearchType.Plasma)
+                    Plasma = Convert.ToInt32(level[1]);
+                else if (type == ResearchType.Power)
+                    Power = Convert.ToInt32(level[1]);
+                else if (type == ResearchType.Bluespace)
+                    Bluespace = Convert.ToInt32(level[1]);
+                else if (type == ResearchType.Biological)
+                    Biological = Convert.ToInt32(level[1]);
+                else if (type == ResearchType.Combat)
+                    Combat = Convert.ToInt32(level[1]);
+                else if (type == ResearchType.EMP)
+                    EMP = Convert.ToInt32(level[1]);
+                else if (type == ResearchType.Data)
+                    Data = Convert.ToInt32(level[1]);
+                else if (type == ResearchType.Illegal)
+                    Illegal = Convert.ToInt32(level[1]);
+                else if (type == ResearchType.Abductor)
+                    Abductor = Convert.ToInt32(level[1]);
+            }
+        }
+    }
+
+    public enum ResearchType
+    {
+        Empty, //0
+        Materials, //1
+        Engineering, //2
+        Plasma, //3
+        Power, //4
+        Bluespace, //5
+        Biological, //6
+        Combat, //7
+        EMP, //8
+        Data, //9
+        Illegal, //10
+        Abductor //11
+    }
+}
